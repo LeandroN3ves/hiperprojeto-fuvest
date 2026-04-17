d922d0fb4404144ef3350079ec54ea38c32c833f
Leandro Neves <leandro_official1@outlook.com>
2026-04-17 18:11:20 -0300

    atualizando url da api no frontend com dominio publico

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hiperprojeto-fuvest.railway.internal' // Note: This internal URL may not work on the client side.
+  apiUrl: 'https://hiperprojeto-fuvest-production.up.railway.app'
 };
